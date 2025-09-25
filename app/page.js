411ea4882852b8ad079c2a976931d1f7035a4b1a
Spencer Muller <spencer.muller@edu.sait.ca>
2025-09-25 17:04:38 -0600

    had to fix again

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        Go to Week 2 <Link href="week-2/page.js">→ </Link>
-        Go to Week 3 <Link href="week-3/page.js">→ </Link> Go to Week 4 → Go to Week 5 → Go to Week 6 → Go to Week 7
+        Go to Week 2 <Link href="week-2">→ </Link>
+        Go to Week 3 <Link href="week-3">→ </Link> Go to Week 4 → Go to Week 5 → Go to Week 6 → Go to Week 7
         → Go to Week 8 →
       </p>
     </main>
