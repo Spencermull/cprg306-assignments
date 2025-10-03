30f3c798a2b4d9c162de945df22e311c1df4c8fe
Spencer Muller <spencer.muller@edu.sait.ca>
2025-10-03 17:24:10 -0600

    Fixed buttons and added some styling to the html

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -16,16 +16,25 @@ export default function NewItem() {
   }
 
   return (
-    <main>
-      <h1>Current Quantity: {quantity}</h1>
-      <ul>
-        <li>
-          <button onClick={Increment}> Add </button>
-        </li>
-        <li>
-          <button onClick={Decrement}> Subtract </button>
-        </li>
-      </ul>
-    </main>
+    <section className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="mb-4 text-xl">Current Quantity: {quantity}</h1>
+      <div className="flex gap-4 bg-gradient-to-br from-sky-400 to-blue-700 p-4 rounded">
+        <button
+          type="button"
+          onClick={Increment}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          onClick={Decrement}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Subtract
+        </button>
+        <h2>Range is from 1-20</h2>
+      </div>
+    </section>
   );
 }
